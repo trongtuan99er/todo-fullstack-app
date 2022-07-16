69ec3df0997834e6fe56cee600f450bfe593f6cf
trongtuan99er <trongtuan681@gmail.com>
2022-07-16 13:23:18 +0700

    fisrt commit

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.send("hello ae")
+});
+
+app.post("/name", (req,res) => {
+  if(req.body.name){
+    return res.json({name: req.body.name});
+  }else {
+    return res.status(400).json({error : "no name "});
+  }
+})
+app.listen(process.env.PORT, ()=> {
+  console.log(`server run on port ${process.env.PORT}`);
+})
