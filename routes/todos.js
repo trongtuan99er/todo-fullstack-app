f986cfe72b5d2b5a6d3072bf3a50f81ff5bed514
trongtuan99er <trongtuan681@gmail.com>
2022-07-17 10:24:27 +0700

    make todo as complete

--- routes/todos.js
+++ routes/todos.js
@@ -40,7 +40,7 @@ router.post("/new", requiresAuth, async (req, res) => {
 });
 
 // @route   GET /apt/todos/current
-// @desc    Current user todos
+// @desc    Get Current user todo list
 // @acess   Private
 router.get("/current",requiresAuth, async (req, res) => {
   try {
@@ -60,6 +60,44 @@ router.get("/current",requiresAuth, async (req, res) => {
       incomplete: incompleteTodos,
       complete: completeTodos
     })
+  }catch(err) {
+    console.log(err);
+    return res.status(500).send(err.message)
+  }
+})
+
+// @route   PUT /apt/todos/:todoId/complete
+// @desc    Make todo to complete state
+// @acess   Private
+
+router.put("/:todoId/complete", requiresAuth, async(req, res) => {
+  try{ 
+    const todo = await ToDo.findOne({
+      user: req.user._id,
+      _id: req.params.todoId
+    });
+
+    if(!todo) {
+      return res.status(404).json({error: "Không thể tìm thấy việc cần làm"})
+    }
+    if(todo.complete) {
+      return res.status(400).json({error: "việc cần làm đã hoàn thành "})
+    }
+
+    const updatedTodo = await ToDo.findOneAndUpdate(
+      {
+        user: req.user._id,
+        _id: req.params.todoId
+      },
+      {
+        complete: true,
+        completeAt: new Date()
+      },
+      {
+        new: true
+      }
+    )
+    return res.json(updatedTodo);
   }catch(err) {
     console.log(err);
     return res.status(500).send(err.message)
