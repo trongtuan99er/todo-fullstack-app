30297b20990e510e4a658d45ba79dd631b9204a8
trongtuan99er <trongtuan681@gmail.com>
2022-07-23 18:42:08 +0700

    render todo list on dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,30 @@
 import React from "react";
 import { useGlobalContext } from "../context/GolobalContext";
 import { useNavigate } from "react-router-dom";
+import { makeStyles } from '@mui/styles';
+import { Box, Typography } from "@mui/material"
+import TodoCard from "./TodoCard";
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 768,
+    margin: '0 auto',
+    width: '100%',
+    padding: 16,
+  },
+  todos: {
+    '&:not(:last-child)': {
+      marginBottom: 30
+    }
+  },
+  todoTitle: {
+    fontSize: '2rem',
+    marginBottom: 15,
+  }
+})
 function Dashboard() {
-  const { user } = useGlobalContext();
+  const classes = useStyles()
+  const { user, completeTodos, incompleteTodos } = useGlobalContext();
   const navigate = useNavigate();
 
   React.useEffect(()=> {
@@ -12,7 +33,21 @@ function Dashboard() {
     }
   },[user, navigate])
   return ( 
-    <h1>DashBoard</h1>
+    <Box className={classes.root}>
+      <Box className={classes.todos}>
+        {incompleteTodos.map((todo) => (
+          <TodoCard todo={todo} key={todo._id}/>
+        ))}
+      </Box>
+
+      {completeTodos.length > 0 && 
+        <Box className={classes.todos}>
+        <Typography className={classes.todoTitle}>Công việc đã xong:</Typography>
+        {completeTodos.map((todo)=> (
+          <TodoCard todo={todo} key={todo._id}/>
+        ))}
+      </Box>}
+    </Box>
   );
 }
 
