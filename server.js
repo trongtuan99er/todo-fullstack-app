bb8264f10acc31ffe6a639594f311ad93d2cd4c5
trongtuan99er <trongtuan681@gmail.com>
2022-07-23 21:36:09 +0700

    prepare for deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser")
-
+const path = require('path')
 // import routes
 
 const authRoute = require("./routes/auth")
@@ -20,6 +20,11 @@ app.get("/api", (req, res) => {
 
 app.use("/api/auth", authRoute)
 app.use("/api/todos", todosRoute)
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", (res, req) => {
+  res.sendFile(path.resolve(__dirname,"./client/build", "index.html"));
+})
 
 mongoose.connect(process.env.MONGO_URL).then(()=> {
   console.log("conect to mongo db");
